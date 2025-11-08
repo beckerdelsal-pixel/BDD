d6b80a7cc1ad11faa6988cfcd1b1968238e91616
beckerdelsal-pixel <beckerdelsal@gmail.com>
2025-11-07 22:42:13 -0500

    creacion de la tabla transacciones

---
+++ Scripts/script_transacciones.sql
@@ -0,0 +1,10 @@
+--crear tabla transacciones
+create table transacciones(
+	codigo int not null,
+	numero_cuenta char(5) not null,
+	monto money not null,
+	tipo char(1) not null,
+	fecha date not null,
+	hora time not null,
+	constraint codigoTransacciones_pk primary key(codigo)
+)
