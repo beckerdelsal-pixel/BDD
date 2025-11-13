6d09b33244d0bcf78a4e6e75cc55f516723362dd
beckerdelsal-pixel <beckerdelsal@gmail.com>
2025-11-13 00:13:04 -0500

    prueba de consultas clase

---
+++ Scripts/pruebas de consultas.sql
@@ -0,0 +1,5 @@
+select * from personas where fecha_nacimiento between '10/06/1995' 
+and '22/05/2000'
+
+insert into personas (cedula,nombre,apellido,fecha_nacimiento)
+values('0714616123','Jefferson','Farfan','10/06/1995')
