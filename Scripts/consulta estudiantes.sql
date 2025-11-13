66b5693d0ec241597a8bdb633710bb3ef78782d8
beckerdelsal-pixel <beckerdelsal@gmail.com>
2025-11-13 11:11:40 -0500

    se consulta tabla estudiantes

---
+++ Scripts/consulta estudiantes.sql
@@ -0,0 +1,6 @@
+--trae nombre y cedula de los estudiantes
+select nombre,cedula from estudiantes
+--nombre de los estudiantes cuya cedula empiece con 17
+select nombre from estudiantes where cedula like '17%'
+--nombre completo de los estudiantes cuyo nombre empiece con A
+select nombre,apellido from estudiantes where nombre like 'A%'
--- Scripts/script_estudiantes.sql
+++ Scripts/script_estudiantes.sql
@@ -1,4 +1,5 @@
 -- crear tabla estudiantes
+drop table estudiantes
 create table estudiantes(
 	cedula char(10) not null,
 	nombre varchar(50) not null,
@@ -8,9 +9,9 @@ create table estudiantes(
 	constraint estudiantes_pk primary key(cedula)
 )
 insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
-values('0998544636','Juanito','Alimaña','juanitoalim@hotmail.com','10/05/2002')
+values('1798544636','Juanito','Alimaña','juanitoalim@hotmail.com','10/05/2002')
 insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
-values('1234556453','Pedro','Alchundia','pedroalchundia79@gmail.com','14/10/1979')
+values('1734556453','Pedro','Alchundia','pedroalchundia79@gmail.com','14/10/1979')
 insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
 values('0941088304','Becker','Del Salto','beckerdelsalto@gmail.com','29/06/2005')
 insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
@@ -22,5 +23,5 @@ values('0922144266','Leidy','Leon','rosseleon18@hotmail.com','18/09/1979')
 insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
 values('0887996547','Alan','Brito','alanbritodelgado24@yahoo.com','24/09/1999')
 insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
-values('0978663218','Susana','Oria','susanaoriablanca@outlook.com','24/12/2015')
+values('1778663218','Susana','Oria','susanaoriablanca@outlook.com','24/12/2015')
 select * from estudiantes
