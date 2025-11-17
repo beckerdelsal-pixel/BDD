8ea3ffe9f36d295ff316a954d2f31f9e1f8dc3ef
beckerdelsal-pixel <beckerdelsal@gmail.com>
2025-11-17 15:36:43 -0500

    funciones creadas, llamadas desde TestCalculadora y ejecutadas

--- Clases_Java/src/com/krakedev/TestCalculadora.java
+++ Clases_Java/src/com/krakedev/TestCalculadora.java
@@ -8,11 +8,30 @@ public class TestCalculadora {
 		Calculadora calcu2 = new Calculadora();
 		int resultadoSuma;
 		resultadoSuma = calcu1.sumar(5, 3);
+		System.out.println("**********SUMA**********");
 		System.out.println("Resultado Suma: " + resultadoSuma);
 		int resultadoResta;
 		resultadoResta = calcu2.restar(10, 3);
+		System.out.println("**********RESTA**********");
 		System.out.println("Resultado Resta: " + resultadoResta);
-		
+		//prueba multiplicar
+		double resultadoMultiplicacion;
+		resultadoMultiplicacion = calcu1.multiplicar(10, 5);
+		System.out.println("**********MULTIPLICACION**********");
+		System.out.println("Resultado Multiplicar: " + resultadoMultiplicacion);
+		//prueba dividir
+		double resultadoDivision;
+		resultadoDivision = calcu1.dividir(10, 2);
+		System.out.println("**********DIVISION**********");
+		System.out.println("Resultado Dividir: " + resultadoDivision);
+		//prueba promediar
+		double resultadoPromedio;
+		resultadoPromedio = calcu2.promediar(10, 8, 9);
+		System.out.println("**********PROMEDIOS**********");
+		System.out.println("Resultado Promediar: " + resultadoPromedio);
+		//prueba mostrarResultado
+		System.out.println("**********MOSTRAR RESULTADO MENSAJE**********");
+		calcu2.mostrarResultado();
 
 	}
 
