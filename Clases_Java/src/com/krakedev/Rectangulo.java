989aec46277ed44e630c15bc0075beb1da28f580
beckerdelsal-pixel <beckerdelsal@gmail.com>
2025-11-17 19:38:50 -0500

    Terminado calcular area y perimetro del rectangulo

---
+++ Clases_Java/src/com/krakedev/Rectangulo.java
@@ -0,0 +1,18 @@
+package com.krakedev;
+
+public class Rectangulo {
+	public int base;
+	public int altura;
+	
+	public int calcularArea() {
+		int area = 0;
+		area = base * altura;
+		return  area;
+	}
+	
+	public int calcularPerimetro() {
+		int perimetro = 0;
+		perimetro = (base + altura) * 2;
+		return perimetro;
+	}
+}
---
+++ Clases_Java/src/com/krakedev/TestRectangulo.java
@@ -0,0 +1,30 @@
+package com.krakedev;
+
+public class TestRectangulo {
+
+	public static void main(String[] args) {
+		// 
+		Rectangulo r1 = new Rectangulo();
+		Rectangulo r2 = new Rectangulo();
+		r1.base = 10;
+		r1.altura = 5;
+		
+		int area = r1.calcularArea();
+		System.out.println("Area: " + area);
+		
+		int perimetro = r1.calcularPerimetro();
+		System.out.println("Perimetro: " + perimetro);
+		
+		r2.base = 8;
+		r2.altura = 3;
+		
+		int area2 = r2.calcularArea();
+		System.out.println("Area: " + area2);
+		
+		int perimetro2 = r2.calcularPerimetro();
+		System.out.println("Perimetro: " + perimetro2);
+		
+		
+	}
+
+}
